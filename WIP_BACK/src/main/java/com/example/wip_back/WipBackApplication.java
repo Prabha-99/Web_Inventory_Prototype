a3c89c492004ce26bd8156db40354cc2e7deed81
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-02-12 13:16:33 +0530

    Initial Commit

---
+++ WIP_BACK/src/main/java/com/example/wip_back/WipBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.wip_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WipBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WipBackApplication.class, args);
+    }
+
+}
---
+++ WIP_BACK/src/test/java/com/example/wip_back/WipBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wip_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WipBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
