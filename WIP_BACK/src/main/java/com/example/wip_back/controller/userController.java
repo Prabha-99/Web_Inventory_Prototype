bddb1319d534a0180eee4d5dcefe04e07e22cf48
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-02-13 23:23:32 +0530

    Added CrossOrigin link to usersController class

--- WIP_BACK/src/main/java/com/example/wip_back/controller/userController.java
+++ WIP_BACK/src/main/java/com/example/wip_back/controller/userController.java
@@ -2,6 +2,7 @@ package com.example.wip_back.controller;
 
 import com.example.wip_back.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.example.wip_back.service.userService;
 
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api/tables")
 public class userController {
