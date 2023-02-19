dc6fa52a40f329f7ee9f81620cb7255519bee3f6
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-02-19 14:23:31 +0530

    Modified the users component

--- WIP_FRONT/src/app/users/users.component.ts
+++ WIP_FRONT/src/app/users/users.component.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
 
   users:any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http:HttpClient) { }
 
 
   ngOnInit(){
-    let response= this.http.get("http://localhost:8080/api/tables"); //Here
+    let response= this.http.get("http://localhost:8080/api/tables/allUsers"); //Here
     response.subscribe((data)=>this.users=data);
  }
 
