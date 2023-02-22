c2d4530edb8234f7867b5a8161bb86b2e0935631
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-02-22 23:43:55 +0530

    Add some html into products component and added Angular Routings

--- WIP_FRONT/src/app/app-routing.module.ts
+++ WIP_FRONT/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { UsersComponent } from './users/users.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path:"navbar",component:NavbarComponent},
   {path:"sidebar",component:SidebarComponent},
   {path:"users",component:UsersComponent},
+  {path:"products",component:ProductsComponent}
 ];
 
 @NgModule({
