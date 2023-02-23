d4a9d1135859d37c6cf2bfd973959c8642f3b0f9
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-02-23 22:45:36 +0530

    Added HTML login form to login component and did some customizations

--- WIP_FRONT/src/app/app-routing.module.ts
+++ WIP_FRONT/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductsComponent } from './products/products.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path:"navbar",component:NavbarComponent},
   {path:"sidebar",component:SidebarComponent},
   {path:"users",component:UsersComponent},
-  {path:"products",component:ProductsComponent}
+  {path:"products",component:ProductsComponent},
+  {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
